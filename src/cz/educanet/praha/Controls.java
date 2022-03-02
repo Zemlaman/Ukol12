7f502745a6c745db86aaa3913c0c8a91eb0c1ea8
Matěj Žemlička <43809215+Zemlaman@users.noreply.github.com>
2022-03-02 22:24:26 +0100

    Delete src/cz/educanet/praha directory

--- src/cz/educanet/praha/Controls.java
+++
@@ -1,9 +0,0 @@
-package cz.educanet.praha;
-
-public class Controls implements ILanderControls {
-
-    @Override
-    public int getSecondsOfFuelBurn(int altitude, int velocity) {
-        return altitude / velocity;
-    }
-}
